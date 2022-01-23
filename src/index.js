dee963889e5af7f2d87a7d15e126ffa0705b6d80
Luke Diebold <luke@ldiebold.com>
2022-01-23 16:41:42 +1100

    v0.0.3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
-const QuasarVuexOrmPlugin = require('./QuasarVuexOrmPlugin.js')
+const fs = require('fs')
+const { render } = require('template-file')
+const { pascalCase, snakeCase } = require('change-case')
+const pluralize = require('pluralize')
 
 module.exports = function (api) {
+  api.compatibleWith('quasar', '^2.0.0')
+  api.compatibleWith('@quasar/app', '^3.0.0')
 
+  // Add webpack alias for models
+  api.chainWebpack((chain) => {
+    chain.resolve.alias.set('models', api.resolve.src('models'))
+  })
+
+  api.registerCommand('new:model', ({ args, params }) => {
+    const modelPascal = pascalCase(args[0])
+    const modelTable = pluralize(snakeCase(args[0]))
+
+    const file = api.resolve.src('models') + '/' + modelPascal + '.js'
+
+    const result = render(modelTemplate, { modelPascal, modelTable })
+    
+    fs.writeFileSync(file, result)
+  })
 }
 
-exports.QuasarVuexOrmPlugin = QuasarVuexOrmPlugin
+var modelTemplate = `import { Model } from '@vuex-orm/core'
+
+export default class {{ modelPascal }} extends Model {
+  static entity = '{{ modelTable }}'
+
+  static fields () {
+    return {
+      id: this.attr(null)
+    }
+  }
+}
+`
--- src/templates/src/store/QuasarVuexOrmPlugin.js
+++ src/templates/src/store/QuasarVuexOrmPlugin.js
@@ -1,16 +1,16 @@
-const VuexORM = require('@vuex-orm/core')
+import { Database, install } from '@vuex-orm/core'
 
-module.exports = function QuasarVuexOrmPlugin() {
+export default function () {
   /**
    * Here, we initialize the database, and require all js files
    * in our models direcory
    */
-  const database = new VuexORM.Database()
+  const database = new Database()
   const modelFileContext = require.context('src/models', false, /\.js$/)
   modelFileContext.keys().forEach(modelPath => {
     const model = modelFileContext(modelPath)
     database.register(model.default)
   })
 
-  return VuexORM.install(database)
+  return install(database)
 }
